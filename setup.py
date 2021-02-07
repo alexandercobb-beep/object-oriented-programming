f001bde239d83fccca8f702e0e61ca1c0f85aca2
aleexanderjuxoncobb <alexxander.cobb19@imperial.ac.uk>
2021-02-07 12:16:28 +0000

    i think i am done

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 from glob import glob
 
+
 setup(name='oo-programming-exercises',
       version=2020.0,
-      description=("Code for Object oriented programming "
+      description=("Code for Object oriented programming"
                    "in Python for mathematicians."),
       author="David Ham",
       author_email="david.ham@imperial.ac.uk",
